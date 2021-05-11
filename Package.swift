1474823cc1f4dcc791adeb70f81cee724eed42ca
Mina Her <minacle@live.com>
2021-05-11 15:30:32 +0900

    Specify minimum platform versions (#54)
    This resolves #53.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "AnyCodable",
+    platforms:  [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
