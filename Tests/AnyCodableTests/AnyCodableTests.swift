4dadf076ac15f0f1bf2c8768c349b0e7d62547a9
Paul Wilkinson <paulw@wilko.me>
2021-09-12 09:32:44 +1000

    Add support for encoding anything that conforms to `Encodable`

--- Tests/AnyCodableTests/AnyCodableTests.swift
+++ Tests/AnyCodableTests/AnyCodableTests.swift
@@ -1,6 +1,20 @@
 @testable import AnyCodable
 import XCTest
 
+struct SomeCodable: Codable {
+    var string: String
+    var int: Int
+    var bool: Bool
+    var hasUnderscore: String
+    
+    enum CodingKeys: String,CodingKey {
+        case string
+        case int
+        case bool
+        case hasUnderscore = "has_underscore"
+    }
+}
+
 class AnyCodableTests: XCTestCase {
     func testJSONDecoding() throws {
         let json = """
@@ -18,10 +32,10 @@ class AnyCodableTests: XCTestCase {
             "null": null
         }
         """.data(using: .utf8)!
-
+        
         let decoder = JSONDecoder()
         let dictionary = try decoder.decode([String: AnyCodable].self, from: json)
-
+        
         XCTAssertEqual(dictionary["boolean"]?.value as! Bool, true)
         XCTAssertEqual(dictionary["integer"]?.value as! Int, 42)
         XCTAssertEqual(dictionary["double"]?.value as! Double, 3.141592653589793, accuracy: 0.001)
@@ -30,8 +44,11 @@ class AnyCodableTests: XCTestCase {
         XCTAssertEqual(dictionary["nested"]?.value as! [String: String], ["a": "alpha", "b": "bravo", "c": "charlie"])
         XCTAssertEqual(dictionary["null"]?.value as! NSNull, NSNull())
     }
-
+    
     func testJSONEncoding() throws {
+        
+        let someCodable = AnyCodable(SomeCodable(string: "String", int: 100, bool: true, hasUnderscore: "another string"))
+        
         let dictionary: [String: AnyCodable] = [
             "boolean": true,
             "integer": 42,
@@ -43,14 +60,15 @@ class AnyCodableTests: XCTestCase {
                 "b": "bravo",
                 "c": "charlie",
             ],
+            "someCodable": someCodable,
             "null": nil
         ]
-
+        
         let encoder = JSONEncoder()
-
+        
         let json = try encoder.encode(dictionary)
         let encodedJSONObject = try JSONSerialization.jsonObject(with: json, options: []) as! NSDictionary
-
+        
         let expected = """
         {
             "boolean": true,
@@ -63,11 +81,17 @@ class AnyCodableTests: XCTestCase {
                 "b": "bravo",
                 "c": "charlie"
             },
+            "someCodable": {
+                "string":"String",
+                "int":100,
+                "bool": true,
+                "has_underscore":"another string"
+            },
             "null": null
         }
         """.data(using: .utf8)!
         let expectedJSONObject = try JSONSerialization.jsonObject(with: expected, options: []) as! NSDictionary
-
+        
         XCTAssertEqual(encodedJSONObject, expectedJSONObject)
     }
 }
